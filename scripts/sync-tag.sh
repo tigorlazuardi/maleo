f2b7150b97193f7079cc40fb51d5e0314452e254
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2023-01-27 21:34:26 +0700

    chore(script): udpate docs

--- scripts/sync-tag.sh
+++ scripts/sync-tag.sh
@@ -16,20 +16,22 @@ case "$DRONE_COMMIT_MESSAGE" in
 	((MAJ += 1))
 	MIN=0
 	PATCH=0
-	echo "== INFO: #major found, incrementing major version"
+	echo "== INFO: #major found, using next major version"
 	;;
 *"#minor"*)
 	((MIN += 1))
 	PATCH=0
-	echo "== INFO: #minor found, incrementing minor version"
+	echo "== INFO: #minor found, using next minor version"
 	;;
 *)
 	((PATCH += 1))
-	echo "== INFO: incrementing Patch Version"
+	echo "== INFO: using next Patch Version"
 	;;
 esac
 
 NEWTAG="$MAJ.$MIN.$PATCH"
+
+FILES=$(find . -name go.mod | grep -v '^\./go.mod$')
 
 for f in $FILES; do
 	echo "== INFO: Updating $PACKAGE to $NEWTAG"
@@ -38,15 +40,3 @@ for f in $FILES; do
 done
 
 git commit -m "Bump Version to v$NEWTAG [CI SKIP]"
-
-for f in $FILES; do
-	PACKAGE=$(echo "$f" | cut -d/ -f2- | xargs dirname)
-	PACKAGE_TAG="$PACKAGE/v$NEWTAG"
-	echo "== INFO: Adding Tag: $PACKAGE_TAG"
-	git tag "$PACKAGE_TAG"
-done
-
-echo "== INFO: Adding Tag: v$NEWTAG"
-git tag v$NEWTAG
-
-FILES=$(find . -name go.mod | grep -v '^\./go.mod$')
